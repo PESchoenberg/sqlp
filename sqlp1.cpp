a90812822f6f4a0cf9f0dae6c75455cc511787b1
PESchoenberg <pablo.edronkin@yahoo.com>
2020-05-10 15:34:29 -0300

    Maintenance.

--- sqlp1.cpp
+++ sqlp1.cpp
@@ -4,7 +4,7 @@ sqlp1.cpp
 
 ================================================================================
 
- Copyright (C) 2019  Pablo Edronkin (pablo.edronkin at yahoo.com)
+ Copyright (C) 2019 - 2020  Pablo Edronkin (pablo.edronkin at yahoo.com)
 
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU Lesser General Public License as published by
